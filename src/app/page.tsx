8a86adda35254cd398b7e1ac19fffe8d4bdaa9fb
deba7 <debayudhbasu@gmail.com>
2024-11-07 18:35:15 +0530

    hwe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -311,8 +311,8 @@ export default function AcademicRecordsApp() {
     const res = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
-        pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
-        pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
+        pinata_api_key: PINATA_API_KEY,
+        pinata_secret_api_key: PINATA_SECRET_API_KEY,
       },
     });
 
